d7f935a8bf217fd68cde068d60c5c6167e63ded9
johngit23 <126653377+johngit23@users.noreply.github.com>
2024-02-06 22:19:09 +0300

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -29,7 +29,7 @@ def get_locale() -> str:
         str: best match
     """
     locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
+    if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
