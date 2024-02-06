20ca92c3ca6882bc47526b6733bac95de2445e1e
johngit23 <126653377+johngit23@users.noreply.github.com>
2024-02-06 22:16:46 +0300

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -29,7 +29,7 @@ def get_locale() -> str:
         str: best match
     """
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -43,11 +43,8 @@ def index() -> str:
     '''
     return render_template("4-index.html")
 
-# uncomment this line and comment the @babel.localeselector
-# you get this error:
-# AttributeError: 'Babel' object has no attribute 'localeselector'
-# babel.init_app(app, locale_selector=get_locale)
+
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
